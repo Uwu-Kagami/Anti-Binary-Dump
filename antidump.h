72bf07c653ea05695d08e940ec22606016c0f50f
Kagami Dev <PatrickGoolMan1300@proton.me>
2025-03-22 23:20:21 +0100

    Upgrade ( i hope ...)

--- antidump.h
+++ antidump.h
@@ -1,16 +1,58 @@
 #pragma once
 #include <Windows.h>
 #include <winternl.h>
+#include <iostream>
 
-int anti_dump()
-{
-	const auto peb = (PPEB)__readgsqword(0x60);
+#define XOR_KEY 0xAA
 
-	const auto in_load_order_module_list = (PLIST_ENTRY)peb->Ldr->Reserved2[1];
-	const auto table_entry = CONTAINING_RECORD(in_load_order_module_list, LDR_DATA_TABLE_ENTRY, Reserved1[0]);
-	const auto p_size_of_image = (PULONG)&table_entry->Reserved3[1];
-	*p_size_of_image = (ULONG)((INT_PTR)table_entry->DllBase + 0x100000);
+typedef struct _LDR_DATA_TABLE_ENTRY LDR_DATA_TABLE_ENTRY;
+typedef struct _PEB {
+    BYTE Reserved1[2];
+    struct {
+        PVOID Reserved2[2];
+    } Ldr;
+} PEB, *PPEB;
 
-	return 0;
+PPEB GetPeb() {
+    return (PPEB)__readgsqword(0x60);
+}
 
-};
+bool IsDebuggerPresentEx() {
+    BOOL isDebuggerPresent = FALSE;
+    __asm {
+        call IsDebuggerPresent
+        mov isDebuggerPresent, eax
+    }
+    return isDebuggerPresent != FALSE;
+}
+
+void AntiDumpingTools() {
+    if (IsDebuggerPresentEx()) {
+        MessageBoxA(NULL, "Debugger Detected! Exiting...", "Protection", MB_OK);
+        ExitProcess(1);
+    }
+}
+
+void ModifyImageSize() {
+    const auto peb = GetPeb();
+    const auto inLoadOrderModuleList = (PLIST_ENTRY)peb->Ldr.Reserved2[1];
+    const auto tableEntry = CONTAINING_RECORD(inLoadOrderModuleList, LDR_DATA_TABLE_ENTRY, Reserved1[0]);
+    PULONG pSizeOfImage = (PULONG)&tableEntry->Reserved3[1];
+    *pSizeOfImage = (ULONG)((INT_PTR)tableEntry->DllBase + 0x100000);
+}
+
+void EncryptMemory(void* baseAddress, size_t size) {
+    unsigned char* ptr = (unsigned char*)baseAddress;
+    for (size_t i = 0; i < size; ++i) {
+        ptr[i] ^= XOR_KEY;
+    }
+}
+
+void AntiDumpAdvanced() {
+    AntiDumpingTools();
+    ModifyImageSize();
+    const auto baseAddr = (void*)GetModuleHandle(NULL);
+    const size_t size = 0x1000;
+    EncryptMemory(baseAddr, size);
+    EncryptMemory(baseAddr, size);
+}
